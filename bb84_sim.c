812a858daa220458b6be33591454f18160abeb29
harismubashar <97765751+harismubashar@users.noreply.github.com>
2023-11-23 06:34:25 -0500

    Add files via upload

--- bb84_sim.c
+++ bb84_sim.c
@@ -9,99 +9,80 @@ char *enc_basis = NULL;
 char *dec_basis = NULL;
 
 
-// Function to generate a random string
+// Helper function to generate a random basis
 char *generateRandomBasis(int size){
-    int randomNumber;
-    // Allocate memory for the string (including space for the null terminator)
-    char *randomString = malloc((size + 1) * sizeof(char));
+    int randNum;
+    char *randStr = malloc((size+1)*sizeof(char));
 
-    // Check if memory allocation is successful
-    if (randomString == NULL) {
-        perror("Memory allocation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Generate the random string
-    for (int i = 0; i < size; i++) {
-        // Randomly choose "r" or "d" with equal probability
-        randomNumber = rand() % 2;
-
-        if (randomNumber == 0) {
-            randomString[i] = 'r';
+    for (int i = 0; i < size; i++){
+        randNum = rand()%2;
+        if (randNum == 0){
+            randStr[i] = 'r';
         } else {
-            randomString[i] = 'd';
+            randStr[i] = 'd';
         }
     }
-
-    // Add the null terminator at the end
-    randomString[size] = '\0';
-
-    return randomString;
+    randStr[size] = '\0';
+    return randStr;
 }
 
 // Function to generate a random string
 char *generateRandomMessage(int size){
-    int randomNumber;
-    // Allocate memory for the string (including space for the null terminator)
-    char *randomString = malloc((size + 1) * sizeof(char));
+    int randNum;
+    char *randStr = malloc((size+1)*sizeof(char));
 
-    // Check if memory allocation is successful
-    if (randomString == NULL) {
-        perror("Memory allocation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Generate the random string
-    for (int i = 0; i < size; i++) {
-        // Randomly choose "r" or "d" with equal probability
-        randomNumber = rand() % 2;
-
-        if (randomNumber == 0) {
-            randomString[i] = '0';
+    for (int i = 0; i < size; i++){
+        randNum = rand() % 2;
+        if (randNum == 0){
+            randStr[i] = '0';
         } else {
-            randomString[i] = '1';
+            randStr[i] = '1';
         }
     }
-
-    // Add the null terminator at the end
-    randomString[size] = '\0';
-
-    return randomString;
+    randStr[size] = '\0';
+    return randStr;
 }
 
-int isBinaryString(const char *str){
-    while (*str) {
-        if (*str != '0' && *str != '1') {
-            return 0; // Not a binary string
+// Helper function to check if a string is in binary
+int isBinary(const char *str){
+    while (*str){
+        if ((*str != '0') && (*str != '1')){
+            // str is not binary
+            return 0;
         }
         str++;
     }
-    return 1; // Binary string
+    // str is binary
+    return 1;
 }
 
-char generateRandomBit() {
-    // Generate a random number (0 or 1)
-    int randomBit = rand() % 2;
+// Helper function to check if a string is a valid basis
+int isBasis(const char *str){
+    while (*str){
+        if ((*str != 'r') && (*str != 'd')){
+            // str is not a basis
+            return 0;
+        }
+        str++;
+    }
+    // str is a basis
+    return 1;
+}
 
-    if (randomBit == 0){
+// Helper function to generate a random bit of 1 or 0
+char generateRandomBit(){
+    int randBit = rand() % 2;
+    if (randBit == 0){
         return '0';
     } else {
         return '1';
     }
 }
 
+// Helper function to encrypt the message with the encryption basis
 char *encrypt(char *message, char *enc_basis){
-    // Get the length of other string
-    size_t length = strlen(message);
-
-    // Allocate memory for somestr with the same length as other string
+    int length = strlen(message);
     char *enc_str = malloc((length + 1) * sizeof(char));
-
-    // Check if memory allocation is successful
-    if (enc_str == NULL) {
-        perror("Memory allocation failed");
-        exit(EXIT_FAILURE);
-    }
 
     for (int i = 0; message[i] != '\0'; i++){
         if (message[i] == '0'){
@@ -122,20 +103,12 @@ char *encrypt(char *message, char *enc_basis){
 }
 
 
+// Helper function to decrypt the encrypted string with the decryption basis
 char *decrypt(char *enc_str, char *dec_basis){
-    // Get the length of other string
-    size_t length = strlen(enc_str);
+    int length = strlen(enc_str);
 
-    // Allocate memory for somestr with the same length as other string
     char *dec_str = malloc((length + 1) * sizeof(char));
-
     char temp;
-
-    // Check if memory allocation is successful
-    if (dec_str == NULL) {
-        perror("Memory allocation failed");
-        exit(EXIT_FAILURE);
-    }
 
     for (int i = 0; enc_str[i] != '\0'; i++){
         temp = generateRandomBit();
@@ -168,28 +141,19 @@ char *decrypt(char *enc_str, char *dec_basis){
     return dec_str;
 }
 
-int isBasisString(const char *str) {
-    while (*str) {
-        if (*str != 'r' && *str != 'd') {
-            return 0; // Not a basis string
-        }
-        str++;
-    }
-    return 1; // basis string
-}
-
+// Helper function to produce find the shared information between the basis and the bits associated with the matching indices
 void findSharedInformation(char *enc_basis, char *dec_basis){
     int key_exists = 0;
     printf("The indexes for the matching basis are: ");
     int size = strlen(enc_basis);
-    for (int i = 0; i < size; i++) {
-        if (enc_basis[i] == dec_basis[i]) {
+    for (int i = 0; i < size; i++){
+        if (enc_basis[i] == dec_basis[i]){
             key_exists = 1;
             printf("%d ", i);
         }
     }
     if (key_exists == 0){
-        printf(" NONE. There are no matching basis");
+        printf("NONE. There are no matching basis");
     }
     printf("\n");
     printf("The rest of indexes (without matching basis) are discarded for the resulting shared information\n");
@@ -199,8 +163,8 @@ void findSharedInformation(char *enc_basis, char *dec_basis){
     if (key_exists == 0){
         printf("In this case, there is no shared information as no indexes match.");
     }
-    for (int i = 0; i < size; i++) {
-        if (enc_basis[i] == dec_basis[i]) {
+    for (int i = 0; i < size; i++){
+        if (enc_basis[i] == dec_basis[i]){
             printf("%c", message[i]);
         }
     }
@@ -209,22 +173,23 @@ void findSharedInformation(char *enc_basis, char *dec_basis){
 
 int main(int argc, char *argv[]){
     srand(clock());
-    // Check if the correct number of arguments are provided
+    // Usage and sanity check for running the program
     if (!((argc == 1) ||(argc == 2) || (argc == 4) || (argc == 6))){
         printf("Usage: %s <message in binary> -e <encryption basis> -d <decription basis>\n", argv[0]);
         printf("To use random encryption basis, leave \"-e <encryption basis>\" out\n");
         printf("To use random decription basis, leave \"-d <decription basis>\" out\n");
         printf("Input the encryption and decription basis as a string of \"r\" and \"d\" where \"r\" is chosen rectilinear basis and \"d\" is a chosen diagonal basis for a bit\n");
         printf("Chosen basis must have the same string length as the message\n");
-        return 1; // Exit with an error code
+        return 1;
     }
+    // Use user inputted parameters
     if (argc != 1){
         for (int i = 1; i < argc; i++){
             if (i == 1){
                 message = argv[i];
                 message_length = strlen(message);
                 printf("Chosen Alice's message: %s\n", message);
-                if (isBinaryString(message) != 1){
+                if (isBinary(message) != 1){
                     printf("Chosen message is not a binary string. Exiting\n");
                     return 0;
                 }
@@ -232,32 +197,33 @@ int main(int argc, char *argv[]){
                 printf("Rectilinear Basis is represented by \"r\"\n");
                 printf("Diagonal Basis is represented by \"d\"\n");
             }
+            // Use user inputted encryption basis if provided
             if (strcmp(argv[i], "-e") == 0){
-                // Handle encryption string
                 enc_basis = argv[i + 1];
                 printf("Chosen encryption basis: %s\n", enc_basis);
                 if (message_length != strlen(enc_basis)){
                     printf("Chosen encryption basis is not of same length as message. Exiting\n");
                     return 0;
                 }
-                if (isBasisString(enc_basis) != 1){
+                if (isBasis(enc_basis) != 1){
                     printf("Chosen encoding basis is not a valid basis (must only contain characters of \"r\" and \"d\"). Exiting\n");
                     return 0;
                 }
+            // Use user inputted decryption basis if provided
             } else if (strcmp(argv[i], "-d") == 0){
-                // Handle decryption string
                 dec_basis = argv[i + 1];
                 printf("Chosen decryption basis: %s\n", dec_basis);
                 if (message_length != strlen(dec_basis)){
                     printf("Chosen decryption basis is not of same length as message. Exiting\n");
                     return 0;
                 }
-                if (isBasisString(dec_basis) != 1){
+                if (isBasis(dec_basis) != 1){
                     printf("Chosen decryption basis is not a valid basis (must only contain characters of \"r\" and \"d\"). Exiting\n");
                     return 0;
                 }
             }
         }
+    // Use random parameters if no arguments were given
     } else if (argc == 1){
         // Generate a random number from 10 to 100 to set message length
         message_length = rand()%91;
@@ -266,7 +232,7 @@ int main(int argc, char *argv[]){
         printf("Randomly chosen Alice's message of length %d: %s\n", message_length, message);
         printf("Note: You may also specify protocol details by using the format:\n");
         printf("    Usage: %s <message in binary> -e <encryption basis> -d <decription basis>\n", argv[0]);
-        if (isBinaryString(message) != 1){
+        if (isBinary(message) != 1){
             printf("Chosen message is not a binary string. Exiting\n");
             return 0;
         }
